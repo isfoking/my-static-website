126f015499475beaf720bf1bcef026f3bacd7770
Li Hao <1787046485@qq.com>
2024-09-14 14:27:20 +0800

    1

--- installPwa.js
+++ installPwa.js
@@ -1,3 +1,18 @@
+var deferredPrompt = null;
+
+// 监听beforeinstallprompt事件，该事件在网站满足PWA安装条件时触发，保存安装事件
 window.addEventListener("beforeinstallprompt", (e) => {
-  console.log("触发", e);
+  e.preventDefault();
+  deferredPrompt = e;
+  console.log("触发可下载");
 });
+
+// 监听appinstalled事件，该事件在用户同意安装后触发，清空安装事件
+window.addEventListener("appinstalled", () => {
+  deferredPrompt = null;
+});
+
+// 手动触发PWA安装
+function addToDesktop() {
+  deferredPrompt.prompt();
+}
