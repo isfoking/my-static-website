0ffc2186cc038aa5609d784c1294228da07aecd2
Li Hao <1787046485@qq.com>
2024-09-14 16:34:16 +0800

    12321

--- registerSW.js
+++
@@ -1,8 +0,0 @@
-// This is where we register the PWA service worker.
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./sw.js').then(function(registration) {
-    console.log('Service Worker registered with scope:', registration.scope);
-  }).catch(function(error) {
-    console.log('Service Worker registration failed:', error);
-  });
-}
