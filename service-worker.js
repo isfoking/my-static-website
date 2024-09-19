a603cd7d8c6af4cf6a09655b255f1ac387217be9
Li Hao <1787046485@qq.com>
2024-09-19 09:53:56 +0800

    21321

--- service-worker.js
+++ service-worker.js
@@ -3,6 +3,7 @@
 const CACHE = "pwabuilder-precachev2";
 const precacheFiles = [
   /* Add an array of files to precache for your app */
+  
 ];
 
 self.addEventListener("install", function (event) {
@@ -11,7 +12,10 @@ self.addEventListener("install", function (event) {
   console.log("[PWA Builder] Skip waiting on install");
   self.skipWaiting();
 
-  event.waitUntil(caches.open(CACHE).then(function (cache) {}));
+  event.waitUntil(
+    caches.open(CACHE).then(function (cache) {
+    })
+  );
 });
 
 // Allow sw to control of current page
@@ -21,8 +25,12 @@ self.addEventListener("activate", function (event) {
 });
 
 // If any fetch fails, it will look for the request in the cache and serve it from there first
-self.addEventListener("fetch", function (event) {});
+self.addEventListener("fetch", function (event) {
+ 
+});
 
-function fromCache(request) {}
+function fromCache(request) {
+}
 
-function updateCache(request, response) {}
+function updateCache(request, response) {
+}
